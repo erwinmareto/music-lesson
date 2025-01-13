d9c5f0abc68a0c0dce516f9c4e3f2cfa1cad347f
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2025-01-14 02:03:34 +0700

    feat: created lessons chart

---
+++ src/components/parts/Charts/LessonChart/index.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Label, Pie, PieChart } from "recharts";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const LessonChart = () => {
+  const lessonData = [
+    { status: "attended", total: 20, fill: "var(--color-success)" },
+    { status: "absent", total: 37, fill: "var(--color-danger)" },
+  ];
+
+  const chartConfig = {
+    success: {
+      label: "Attended",
+      color: "hsl(var(--chart-2))",
+    },
+    danger: {
+      label: "Absent",
+      color: "hsl(var(--chart-1))",
+    },
+  } satisfies ChartConfig;
+
+  const totalLessons = lessonData.reduce((acc, curr) => acc + curr.total, 0);
+
+  return (
+    <Card className="flex flex-col">
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={lessonData}
+              dataKey="total"
+              nameKey="status"
+              innerRadius={60}
+              strokeWidth={5}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-3xl font-bold"
+                        >
+                          {totalLessons.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Lessons
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default LessonChart;
