7ff2ecbcc88dca1b9b2f80dcf9b14bc7aab24a3c
erwinmareto <erwinmaretow@gmail.com>
2025-01-15 02:05:57 +0700

    feat: created functions to fetch lesson count by status for lesson donut chart

--- src/lib/constants/queryKeys.ts
+++ src/lib/constants/queryKeys.ts
@@ -2,6 +2,8 @@ export const INSTRUMENT_COUNT_QUERY_KEY = "instrumentCount";
 
 export const LESSON_COUNT_QUERY_KEY = "lessonsCount";
 
+export const LESSON_COUNT_GROUP_QUERY_KEY = "lessonsGroupCount";
+
 export const PACKAGE_COUNT_QUERY_KEY = "packageCount";
 
 export const REVENUE_SUM_QUERY_KEY = "revenueSum";
--- src/queries/lessons/index.ts
+++ src/queries/lessons/index.ts
@@ -2,8 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 
 import directus from "@/lib/directus";
 import { readItems } from "@directus/sdk";
-import { getLessonCount } from "@/repositories/lessons";
-import { LESSON_COUNT_QUERY_KEY } from "@/lib/constants/queryKeys";
+import { getLessonByStatusCount, getLessonCount } from "@/repositories/lessons";
+import {
+  LESSON_COUNT_GROUP_QUERY_KEY,
+  LESSON_COUNT_QUERY_KEY,
+} from "@/lib/constants/queryKeys";
 
 export const useLessons = (page: number) => {
   const result = useQuery({
@@ -28,6 +31,15 @@ export const useLessonCount = () => {
   const result = useQuery({
     queryKey: [LESSON_COUNT_QUERY_KEY],
     queryFn: getLessonCount,
+  });
+
+  return result;
+};
+
+export const useLessonByStatusCount = () => {
+  const result = useQuery({
+    queryKey: [LESSON_COUNT_GROUP_QUERY_KEY],
+    queryFn: getLessonByStatusCount,
   });
 
   return result;
