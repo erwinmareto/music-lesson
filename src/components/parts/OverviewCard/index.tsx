4f0c2d88a6c43dffef731fa3cd1bc10af11e4d4c
erwinmareto <erwinmaretow@gmail.com>
2025-01-14 23:55:02 +0700

    feat: added an overview card variation for user data

--- src/components/parts/OverviewCard/index.tsx
+++ src/components/parts/OverviewCard/index.tsx
@@ -1,10 +1,17 @@
 import { Card } from "@/components/ui/card";
 import { OverviewCardProps } from "./types";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
-const OverviewCard = ({ icon, category, total }: OverviewCardProps) => {
+const OverviewCard = ({ icon, category, total, isUser }: OverviewCardProps) => {
   return (
-    <Card className="grid col-span-12 items-end gap-2 p-4 md:col-span-6 lg:col-span-3 max-sm:flex">
+    <Card
+      className={cn(
+        isUser
+          ? "flex items-end p-2 gap-2 md:gap-4"
+          : "grid col-span-12 items-end gap-2 p-4 md:col-span-6 lg:col-span-3 max-sm:flex",
+      )}
+    >
       <div className="w-10 h-10 flex justify-center items-center bg-slate-200 rounded-lg">
         {icon}
       </div>
