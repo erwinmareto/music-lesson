9bfe21c69d51f794ece9935f604670e65ba3e408
erwinmareto <erwinmaretow@gmail.com>
2025-01-15 02:13:11 +0700

    feat: used lesson count by status data to lesson chart

--- src/components/parts/Charts/LessonChart/index.tsx
+++ src/components/parts/Charts/LessonChart/index.tsx
@@ -2,19 +2,32 @@
 
 import { Label, Pie, PieChart } from "recharts";
 
-import { Card, CardContent } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
+import { useLessonByStatusCount } from "@/queries/lessons";
 
 const LessonChart = () => {
-  const lessonData = [
-    { status: "attended", total: 20, fill: "var(--color-success)" },
-    { status: "absent", total: 37, fill: "var(--color-danger)" },
-  ];
+  const { data: lessonByStatus } = useLessonByStatusCount();
+
+  const realData = lessonByStatus?.map((item) => ({
+    status: item.status,
+    total: parseInt(item.count as string),
+    fill:
+      item.status === "attended"
+        ? "var(--color-success)"
+        : "var(--color-danger)",
+  }));
 
   const chartConfig = {
     success: {
@@ -27,10 +40,14 @@ const LessonChart = () => {
     },
   } satisfies ChartConfig;
 
-  const totalLessons = lessonData.reduce((acc, curr) => acc + curr.total, 0);
+  const totalLessons = realData?.reduce((acc, curr) => acc + curr.total, 0);
 
   return (
     <Card className="flex flex-col">
+      <CardHeader>
+        <CardTitle>Lesson Attendance</CardTitle>
+        <CardDescription>Attendance percentage of all lessons</CardDescription>
+      </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
           config={chartConfig}
@@ -42,7 +59,7 @@ const LessonChart = () => {
               content={<ChartTooltipContent hideLabel />}
             />
             <Pie
-              data={lessonData}
+              data={realData}
               dataKey="total"
               nameKey="status"
               innerRadius={60}
@@ -63,7 +80,7 @@ const LessonChart = () => {
                           y={viewBox.cy}
                           className="fill-foreground text-3xl font-bold"
                         >
-                          {totalLessons.toLocaleString()}
+                          {totalLessons?.toLocaleString()}
                         </tspan>
                         <tspan
                           x={viewBox.cx}
