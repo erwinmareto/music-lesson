9925633c2a8915159f6cc9f42033326983c07007
erwinmareto <erwinmaretow@gmail.com>
2025-01-17 16:41:44 +0700

    feat: created functions for fetching packages data with filter

--- src/lib/constants/queryKeys.ts
+++ src/lib/constants/queryKeys.ts
@@ -10,6 +10,8 @@ export const LESSON_COUNT_QUERY_KEY = "lessonsCount";
 
 export const LESSON_COUNT_GROUP_QUERY_KEY = "lessonsGroupCount";
 
+export const PACKAGE_QUERY_KEY = "package";
+
 export const PACKAGE_COUNT_QUERY_KEY = "packageCount";
 
 export const REVENUE_SUM_QUERY_KEY = "revenueSum";
--- src/queries/packages/index.ts
+++ src/queries/packages/index.ts
@@ -1,14 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
-import { getPackageCount, getTopInstruments } from "@/repositories/packages";
+import {
+  getPackageCount,
+  getPackages,
+  getTopInstruments,
+} from "@/repositories/packages";
 import {
   PACKAGE_COUNT_QUERY_KEY,
+  PACKAGE_QUERY_KEY,
   TOP_INSTRUMENTS_QUERY_KEY,
 } from "@/lib/constants/queryKeys";
+import { Filters, getFilters, getFiltersQueryKeys } from "@/lib/filter";
 
-export const usePackageCount = () => {
+export const usePackageCount = (filters?: Filters[]) => {
+  const packageCountFilters = filters ? getFilters(filters) : {};
+
+  const filtersQueryKeys = filters ? getFiltersQueryKeys(filters) : [];
   const result = useQuery({
-    queryKey: [PACKAGE_COUNT_QUERY_KEY],
-    queryFn: getPackageCount,
+    queryKey: [PACKAGE_COUNT_QUERY_KEY, ...filtersQueryKeys],
+    queryFn: () => getPackageCount(packageCountFilters),
   });
 
   return result;
@@ -18,6 +27,19 @@ export const useTopInstruments = () => {
   const result = useQuery({
     queryKey: [TOP_INSTRUMENTS_QUERY_KEY],
     queryFn: getTopInstruments,
+  });
+
+  return result;
+};
+
+export const usePackages = (page: number, filters: Filters[]) => {
+  const packageFilters = getFilters(filters);
+
+  const filtersQueryKeys = getFiltersQueryKeys(filters);
+
+  const result = useQuery({
+    queryKey: [PACKAGE_QUERY_KEY, page, ...filtersQueryKeys],
+    queryFn: () => getPackages(page, packageFilters),
   });
 
   return result;
